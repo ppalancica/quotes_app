8c83359a75a73d83f5a47ffa2d634788e9e11df4
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-03 15:12:21 +0300

    Added QuoteService class for downloading Quotes from the API

---
+++ lib/services/quote_service.dart
@@ -0,0 +1,20 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/quote.dart';
+
+class QuoteService {
+  static const _apiUrl = 'https://dummyjson.com/quotes';
+
+  Future<List<Quote>> fetchQuotes() async {
+    final response = await http.get(Uri.parse(_apiUrl));
+
+    if (response.statusCode == 200) {
+      final List quotesJson = json.decode(response.body)['quotes'];
+      final List<Quote> quotes =
+          quotesJson.map((q) => Quote.fromJson(q)).toList();
+      return quotes;
+    } else {
+      throw Exception('Failed to load quotes');
+    }
+  }
+}
