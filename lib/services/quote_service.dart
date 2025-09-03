7c79cd83ba8633828e7a2487f31662fc700d12e1
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-03 17:08:19 +0300

    Added Caching related methods to QuoteService

--- lib/services/quote_service.dart
+++ lib/services/quote_service.dart
@@ -30,4 +30,20 @@ class QuoteService {
     final ids = prefs.getStringList('favorite_ids') ?? [];
     return ids.map(int.parse).toList();
   }
+
+  Future<void> cacheQuotes(List<Quote> quotes) async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonQuotes = quotes
+      .map((quote) => json.encode(quote.toJson()))
+      .toList();
+    await prefs.setStringList('cached_quotes', jsonQuotes);
+  }
+
+  Future<List<Quote>> getCachedQuotes() async {
+    final prefs = await SharedPreferences.getInstance();
+    final jsonQuotes = prefs.getStringList('cached_quotes') ?? [];
+    return jsonQuotes
+      .map((q) => Quote.fromJson(json.decode(q)))
+      .toList();
+  }
 }
